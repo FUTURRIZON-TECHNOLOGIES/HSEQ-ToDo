1307ae481e9759bcd930df40cc1bb705afd37121
Harsh Bhavsar <harshbhavsar511@gmail.com>
2026-04-22 18:38:11 +0530

    Add Activity Task SCSS module declaration

---
+++ src/modules/activityTaskRegister/components/ActivityTaskForm.module.scss.ts
@@ -0,0 +1,15 @@
+require("./ActivityTaskForm.module.css");
+const styles = {
+  formContainer: 'formContainer_2f8c7f1b',
+  toolbar: 'toolbar_2f8c7f1b',
+  title: 'title_2f8c7f1b',
+  actions: 'actions_2f8c7f1b',
+  content: 'content_2f8c7f1b',
+  leftColumn: 'leftColumn_2f8c7f1b',
+  rightColumn: 'rightColumn_2f8c7f1b',
+  section: 'section_2f8c7f1b',
+  btnSave: 'btnSave_2f8c7f1b',
+  btnSecondary: 'btnSecondary_2f8c7f1b'
+};
+
+export default styles;
